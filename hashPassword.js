2d34b09e069501e0e83e5291bc2bb8b4c466285d
devUbaid <devubaid493@gmail.com>
2025-04-29 04:14:34 +0530

    first commit

--- hashPassword.js
+++
@@ -1,7 +0,0 @@
-import bcryptjs from 'bcryptjs';
-
-const password = '@admin2025'; // The plaintext password you want to hash
-
-bcryptjs.hash(password, 10).then(hash => {
-  console.log('Hashed Password:', hash);
-}).catch(err => console.error(err));
