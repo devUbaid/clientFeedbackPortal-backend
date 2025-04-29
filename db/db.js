2c2ba836cdfec4d33587a66efa2cd482e927b9d9
devUbaid <devubaid493@gmail.com>
2025-04-29 18:58:40 +0530

    update

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+    console.log("✅ MongoDB connected Successfully");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error.message);
     process.exit(1); // Stop the server if DB connection fails
